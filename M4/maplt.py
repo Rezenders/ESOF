e95dd986d4ecadb5d768beba83350d5397662656
Rezenders-note <gustavorezendesilva@hotmail.com>
2016-10-06 10:15:34 -0300

    os arquivos em M4 foram transferidos para chap11

--- M4/maplt.py
+++
@@ -1,7 +0,0 @@
-import webbrowser, sys, pyperclip
-if len(sys.argv)>1:
-	address =' '.join(sys.argv[1:])	
-else:
-	address = pyperclip.paste()
-print (address)
-webbrowser.open('https://www.google.com/maps/place/' + address)
